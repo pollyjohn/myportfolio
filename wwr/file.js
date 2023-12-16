466a29595dc235113f372a2464881429f11a4913
pollyjohn <joaotv16@gmail.com>
2023-12-16 16:13:05 +0000

    fixed

--- wwr/file.js
+++ wwr/file.js
@@ -1,6 +1,6 @@
 function setMainMarginTop() {
     const headerHeight = document.getElementById('header').offsetHeight;
-    document.querySelector('main').style.top = '${headerHeight}px';
+    document.querySelector('main').style.marginToptop = '${headerHeight}px';
 }
   
 window.onload = setMainMarginTop;
